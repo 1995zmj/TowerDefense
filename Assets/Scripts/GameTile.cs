c0cd400cb274d26f489ece47f15d5181c6f5bcd8
zmj <minjiangzhao@icloud.com>
2020-03-15 20:30:02 +0800

    。。。

---
+++ Assets/Scripts/GameTile.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class GameTile : MonoBehaviour {
+	
+    [SerializeField]
+    Transform arrow = default;
+    
+    GameTile north, east, south, west;
+    
+    public static void MakeEastWestNeighbors (GameTile east, GameTile west) {
+	    Debug.Assert(
+		    west.east == null && east.west == null, "Redefined neighbors!"
+	    );
+	    west.east = east;
+	    east.west = west;
+    }
+}
